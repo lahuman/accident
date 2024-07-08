ec3b52641d0bb31547a11dc765f1590512f1f547
Daniel Lim <lahuman@daum.net>
2024-07-09 08:55:43 +0900

    feat : token 인증 처리 추가 및 성공 / 실패 메시지 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,33 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Headers, Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateAccidentDto } from './dto/accident.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    private readonly config: ConfigService
+  ) { }
 
   @Post("/accident")
-  async save(@Body() accidentDto: CreateAccidentDto ) {
-    await this.appService.saveAccident(accidentDto);
+  async save(@Headers("Authorization") token: String,
+    @Body() accidentDto: CreateAccidentDto) {
+
+
+      if(`Bearer ${this.config.get("TOKEN")}` !== token) {
+        return {status: "FAIL", message: "Not Authorization Request!"}
+      }
+
+    try {
+      await this.appService.saveAccident(accidentDto);
+    } catch (e) {
+      return { status: "FAIL", message: e.message }
+    }
+    return { status: "OK" }
+
+
   }
-  
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
